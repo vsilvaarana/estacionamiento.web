d5cc94f5d8dca6239d9390c0125ad9cf0742442f
vsilva <vsilva@digiflow.pe>
2024-10-22 18:43:41 -0500

    add components home, search and aboutus

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchComponent } from './pages/search/search.component';
+import { AboutusComponent } from './pages/aboutus/aboutus.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "search", component:SearchComponent},
+  { path: "home", component:HomeComponent},
+  { path: "aboutus", component:AboutusComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
