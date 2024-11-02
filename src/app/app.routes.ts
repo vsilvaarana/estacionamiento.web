f701a0ec2712c462d8eb5619f95a714dc12d0728
jozue2329 <jozue2329@gmail.com>
2024-11-02 18:04:05 -0500

    Se agrega los path para la pagina reserva rapida y home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { Reserva_RapidaComponent } from './pages/reserva-rapida/reserva-rapida.component';
+import { HomeComponent } from './pages/home/home.component';
+
 
 export const routes: Routes = [
     {path: "login", component: LoginComponent},
+    {path: "reserva_rapida", component: Reserva_RapidaComponent},
+    {path: "home", component: HomeComponent},
     {path: "", redirectTo: "login", pathMatch: "full"}
 ];
