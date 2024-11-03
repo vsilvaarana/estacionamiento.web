1f4ea9099a1fc31b84bd1229346aa2da68ab7ef7
jozue2329 <jozue2329@gmail.com>
2024-11-03 04:38:29 -0500

    avance nueva reserva

--- src/app/pages/nueva-reserva/nueva-reserva.component.ts
+++ src/app/pages/nueva-reserva/nueva-reserva.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
-
 @Component({
   selector: 'app-nueva-reserva',
   standalone: true,
@@ -10,6 +9,9 @@ import { Router } from '@angular/router';
   templateUrl: './nueva-reserva.component.html',
   styleUrl: './nueva-reserva.component.css'
 })
+
 export class NuevaReservaComponent {
+    __onReservar(){
+         }
   
-  }
+}
