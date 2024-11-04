b9069a92548b1304737e9c0ecf46e450ea4bd897
jozue2329 <jozue2329@gmail.com>
2024-11-04 12:35:57 -0500

    inclusion de menu al home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { SharedModule } from "../../shared/shared.module";
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  standalone: true,
+  imports: [SharedModule],
+  
 })
 export class HomeComponent { }
